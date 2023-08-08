00ad9e7ce791abdca633fd55dc8fc47375e935b6
John Levermore <john.levermore@zhibek.com>
2023-08-08 17:45:06 +0100

    Change osm-download command to skip OSM data files already downloaded

--- scripts/osm-download.sh
+++ scripts/osm-download.sh
@@ -11,20 +11,29 @@ set -e
 INDEX_URL="https://download.geofabrik.de/index-v1-nogeom.json"
 
 # Accept input variables from command flags
-while getopts a:o: flag
+while getopts a:o:f: flag
 do
     case "${flag}" in
         a) AREA=${OPTARG};;
         o) OUTPUT=${OPTARG};;
+        f) FORCE=${OPTARG};;
     esac
 done
 
 # Fallback to environment vars or defaults for input variables
 AREA="${AREA:=}" # Required. Example: greater_london
 OUTPUT="${OUTPUT:=data/sources/geofabrik_$AREA.osm.pbf}" # Default: data/sources/geofabrik_$AREA.osm.pbf
+FORCE="${FORCE:=false}" # Default: False (i.e. Don't re-download if file already exists)
 
 # Validate input variables are set
 [ -z "$AREA" ] && echo "AREA must be set, either as an environment variable or using -a command flag." && exit 1;
+
+# Skip if OSM data already downloaded
+if [ "$FORCE" == false ] && [ -f "$OUTPUT" ]
+then
+  echo "Skipping process as OSM data already downloaded!"
+  exit 0
+fi
 
 # Search Geofabrik index JSON for area
 AREA_MODIFIED=${AREA//\_/\-} # Replace "_" with "-"
